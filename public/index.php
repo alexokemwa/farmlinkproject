59b47750cea62881da4d525946ff5d89f9f9be80
alexokemwa <okemwanalex@gmail.com>
2024-02-26 12:45:02 +0300

    Defined path to the right controller to load right view

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require "../app/core/init.php";
 $controller = $_GET['page_name'] ?? "home";
 $controller = strtolower($controller);
 
-if(file_exists("../app/controllers/" .$controller . ".php")){
-    require "../app/controllers/" .$controller . ".php";
+if(file_exists("../app/controllers/farmers/" .$controller . ".php")){
+    require "../app/controllers/farmers/" .$controller . ".php";
 }
 else{
     echo "controller not found";
