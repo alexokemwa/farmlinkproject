3a22875b0f1d9f3d3b33b00201719050aa6e4df9
alexokemwa <okemwanalex@gmail.com>
2024-03-02 17:54:11 +0300

    added conditional controlls

--- public/index.php
+++ public/index.php
@@ -4,10 +4,12 @@ require "../app/core/init.php";
 $controller = $_GET['page_name'] ?? "home";
 $controller = strtolower($controller);
 
-if(file_exists("../app/controllers/farmers/" .$controller . ".php")){
-    require "../app/controllers/farmers/" .$controller . ".php";
+if(file_exists("../app/controllers/farmers/" . $controller . ".php")) {
+    require "../app/controllers/farmers/" . $controller . ".php";
+} elseif(file_exists("../app/controllers/farmers/account/" . $controller . ".php")) {
+    require "../app/controllers/farmers/account/" . $controller . ".php";
+} elseif(file_exists("../app/controllers/farmers/reports/" . $controller . ".php")) {
+    require "../app/controllers/farmers/reports/" . $controller . ".php";
+}else {
+    echo "Controller not found";
 }
-else{
-    echo "controller not found";
-}
-
