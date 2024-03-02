87f8ce73b96461aa3e5e24711f4d12edf47df7f7
alexokemwa <okemwanalex@gmail.com>
2024-03-03 00:13:55 +0300

    updated control using conditional statements

--- employees/index.php
+++ employees/index.php
@@ -4,10 +4,14 @@ require "../app/core/init.php";
 $controller = $_GET['page_name'] ?? "home";
 $controller = strtolower($controller);
 
-if(file_exists("../app/controllers/" .$controller . ".php")){
-    require "../app/controllers/" .$controller . ".php";
+if(file_exists("../app/controllers/employees/" .$controller . ".php")){
+    require "../app/controllers/employees/" .$controller . ".php";
+}elseif(file_exists("../app/controllers/employees/account/" .$controller . ".php")){
+    require "../app/controllers/employees/account/" .$controller . ".php";
+}elseif(file_exists("../app/controllers/employees/reports/" .$controller . ".php")){
+    require "../app/controllers/employees/reports/" .$controller . ".php";
 }
 else{
-    echo "controller not found";
+    echo "employees controller not found";
 }
 
