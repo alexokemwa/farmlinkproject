517732d92e9340dcf22dff19a9cafb070008343a
alexokemwa <okemwanalex@gmail.com>
2024-03-02 17:53:05 +0300

    added some comment related to cloud implementation

--- app/views/otherviews/header.view.php
+++ app/views/otherviews/header.view.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo esc(APP_NAME);?></title>
+    <!-- link parts change on cloud implementations -->
     <link rel="stylesheet" href="../asserts/css/bootstrap.min.css">
     <link rel="stylesheet" href="../asserts/css/all.min.css">
+    <link rel="stylesheet" href="../asserts/css/custom.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
