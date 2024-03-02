9b976b5a6846e90aedc53e7630f3b72355858f05
alexokemwa <okemwanalex@gmail.com>
2024-03-02 17:50:02 +0300

    created account views loaded by acoount controllers

--- app/views/farmerViews/account/view.view.php
+++ app/views/farmerViews/account/view.view.php
@@ -1,3 +1,10 @@
 <?php
 //the navbar of home farmers view is requiresd in all farmers view pages
-require 'farmerHome.view.php';
+require views_path("farmerOtherviews/constantnavview");?>
+<div class="container-fluid myclassmargintop" >
+        <h1>view account</h1>
+        <h4><?php echo APP_NAME;?></h4>
+    </div>
+
+<?php require views_path("otherviews/footer");?>
+<?php require views_path("farmerOtherviews/pageFooter");?>
