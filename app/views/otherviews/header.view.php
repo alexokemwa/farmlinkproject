3fba09e51e90f2da3015de4971925d48726b0173
alexokemwa <okemwanalex@gmail.com>
2024-03-03 03:48:55 +0300

    added manager custom css

--- app/views/otherviews/header.view.php
+++ app/views/otherviews/header.view.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../asserts/css/bootstrap.min.css">
     <link rel="stylesheet" href="../asserts/css/all.min.css">
     <link rel="stylesheet" href="../asserts/css/custom.css">
+    <link rel="stylesheet" href="../asserts/css/managercustom.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
