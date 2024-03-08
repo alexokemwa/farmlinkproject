2b3015ee6d6c6c31ecd7988e99091b440086700c
alexokemwa <okemwanalex@gmail.com>
2024-03-08 20:47:17 +0300

    index page for initiation of farmer authetication controlls

--- public/index.php
+++ public/index.php
@@ -1,15 +1,11 @@
 <?php
 require "../app/core/init.php";
 
-$controller = $_GET['page_name'] ?? "home";
+$controller = $_GET['page_name'] ?? "landingpage";
 $controller = strtolower($controller);
 
-if(file_exists("../app/controllers/farmers/" . $controller . ".php")) {
-    require "../app/controllers/farmers/" . $controller . ".php";
-} elseif(file_exists("../app/controllers/farmers/account/" . $controller . ".php")) {
-    require "../app/controllers/farmers/account/" . $controller . ".php";
-} elseif(file_exists("../app/controllers/farmers/reports/" . $controller . ".php")) {
-    require "../app/controllers/farmers/reports/" . $controller . ".php";
-}else {
-    echo "Controller not found";
+if(file_exists("../app/controllers/farmerAuth/" . $controller . ".php")) {
+    require "../app/controllers/farmerAuth/" . $controller . ".php";
+}elseif(file_exists("../app/controllers/farmerAuth/" . $controller . ".php")) {
+    require "../app/controllers/farmerAuth/" . $controller . ".php";
 }
