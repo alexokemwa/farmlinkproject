65f7c52bd6629b2afa1bedff355a2730a3ca13f8
alexokemwa <okemwanalex@gmail.com>
2024-03-08 20:36:22 +0300

    added manager and farer custom css

--- app/views/otherviews/header.view.php
+++ app/views/otherviews/header.view.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="../asserts/css/all.min.css">
     <link rel="stylesheet" href="../asserts/css/custom.css">
     <link rel="stylesheet" href="../asserts/css/managercustom.css">
+    <link rel="stylesheet" href="../asserts/css/farmercustom.css">
+    <link rel="stylesheet" href="../asserts/css/employeecustom.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
