d4e5a70b2d24b90dae716f0ede0c3221fdb9e295
alexokemwa <okemwanalex@gmail.com>
2024-03-08 20:24:39 +0300

    added  authetication farmer view and index footer farmer views

---
+++ app/views/farmerOtherviews/indexpageFooter.view.php
@@ -0,0 +1,16 @@
+<div class="d-block d-sm-none fixed-bottom">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-light  ">
+    <div class="container-fluid">
+      <div class="navbar-nav d-flex justify-content-center" style="overflow-x: auto;">
+        <ul class="navbar-nav mb-lg-0 d-flex flex-row flex-nowrap mx-auto ">
+          <li class="nav-item ">
+            <a class="nav-link btn btn-primary rounded-pill px-5  me-4 mycss-stylebtn" href="#">Signup</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary rounded-pill px-5  mycss-stylebtn" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
