9d70f46f8a158f8371f5af3ddf219b766c5b29d7
alexokemwa <okemwanalex@gmail.com>
2024-03-13 17:03:09 +0300

    added cdn link options

--- app/views/otherviews/header.view.php
+++ app/views/otherviews/header.view.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo esc(APP_NAME);?></title>
     <!-- link parts change on cloud implementations -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="stylesheet" href="../asserts/css/bootstrap.min.css">
     <link rel="stylesheet" href="../asserts/css/all.min.css">
     <link rel="stylesheet" href="../asserts/css/custom.css">
     <link rel="stylesheet" href="../asserts/css/managercustom.css">
     <link rel="stylesheet" href="../asserts/css/farmercustom.css">
     <link rel="stylesheet" href="../asserts/css/employeecustom.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 </head>
 <body>
