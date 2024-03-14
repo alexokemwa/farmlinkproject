ea5d121cdc9d3e121e7ead785cb5985c00663b72
alexokemwa <okemwanalex@gmail.com>
2024-03-15 00:35:14 +0300

    modefied page name for proper pagenames for right launch of the right controller

--- app/views/farmerOtherviews/nav.view.php
+++ app/views/farmerOtherviews/nav.view.php
@@ -27,7 +27,7 @@
             reports
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="?page_name=orders">orders</a></li>
+            <li><a class="dropdown-item" href="?page_name=order">orders</a></li>
             <li><a class="dropdown-item" href="?page_name=payment">payment</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="?page_name=markets">markets</a></li>
