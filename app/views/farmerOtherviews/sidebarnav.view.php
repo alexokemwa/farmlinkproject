c3abb1df8544512a7f354f04e3cf70fed4551d1f
alexokemwa <okemwanalex@gmail.com>
2024-03-15 00:33:45 +0300

    created farmer side bar

---
+++ app/views/farmerOtherviews/sidebarnav.view.php
@@ -0,0 +1,42 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar myclassstylesidebar">
+              <div class="sidebar-sticky mystylesidebarsticky">
+                <ul class="nav flex-column ">
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=home">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=markets">
+                      markets
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=orders">
+                    orders
+                    </a>
+                  </li>
+                  
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    reports</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=order">orders</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=payment">payment</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=markets">markets</a>
+                    </ul>
+                  
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    account</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=view">view</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=update">update</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=delete">delete</a>
+                      </ul>
+                  </li>
+                </ul>
+                </ul>
+              </div>
+            </nav>
+           
