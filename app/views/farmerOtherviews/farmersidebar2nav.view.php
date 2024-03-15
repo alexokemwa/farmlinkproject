cde50e0bae4f683c1e595ab0fb2d3e1d5afbefc1
alexokemwa <okemwanalex@gmail.com>
2024-03-15 19:21:58 +0300

    created farmer home sidebars

---
+++ app/views/farmerOtherviews/farmersidebar2nav.view.php
@@ -0,0 +1,28 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar myclassstylesidebar">
+              <div class="sidebar-sticky mystylesidebarsticky">
+                <ul class="nav flex-column ">
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=orderhistory">
+                       order history
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=activeorders">
+                    active_orders
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=pendingorders">
+                    pending_orders
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=ontransit">
+                    ontransit_orders
+                    </a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </nav>
+           
