14469cb9f3fd3e69ae00c9cc615278651a4c705f
alexokemwa <okemwanalex@gmail.com>
2024-03-15 19:23:10 +0300

    farmer sidebar nav

--- app/views/farmerOtherviews/sidebarnav.view.php
+++ app/views/farmerOtherviews/sidebarnav.view.php
@@ -2,40 +2,26 @@
               <div class="sidebar-sticky mystylesidebarsticky">
                 <ul class="nav flex-column ">
                   <li class="nav-item">
-                    <a class="nav-link  mystlenav" href="?page_name=home">
-                      Home
+                    <a class="nav-link  mystlenav" href="?page_name=orderhistory">
+                       order history
                     </a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link  mystlenav" href="?page_name=markets">
-                      markets
+                    <a class="nav-link  mystlenav" href="?page_name=activeorders">
+                    active_orders
                     </a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link  mystlenav" href="?page_name=orders">
-                    orders
+                    <a class="nav-link  mystlenav" href="?page_name=pendingorders">
+                    pending_orders
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=ontransit">
+                    ontransit_orders
                     </a>
                   </li>
                   
-                  <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    reports</a>
-                    <ul class="dropdown-menu">
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=order">orders</a>
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=payment">payment</a>
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=markets">markets</a>
-                    </ul>
-                  
-                  <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    account</a>
-                    <ul class="dropdown-menu">
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=view">view</a>
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=update">update</a>
-                      <a class="nav-link dropdown-item mystlenav" href="?page_name=delete">delete</a>
-                      </ul>
-                  </li>
-                </ul>
                 </ul>
               </div>
             </nav>
