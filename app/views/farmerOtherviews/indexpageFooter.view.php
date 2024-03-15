09adfdefb47e7f7c9ff62f6ee351b10db971ab2a
alexokemwa <okemwanalex@gmail.com>
2024-03-15 19:22:46 +0300

    modified btn spacing

--- app/views/farmerOtherviews/indexpageFooter.view.php
+++ app/views/farmerOtherviews/indexpageFooter.view.php
@@ -4,10 +4,10 @@
       <div class="navbar-nav d-flex justify-content-center" style="overflow-x: auto;">
         <ul class="navbar-nav mb-lg-0 d-flex flex-row flex-nowrap mx-auto ">
           <li class="nav-item ">
-            <a class="nav-link btn btn-primary rounded-pill px-5  me-4 mycss-stylebtn" href="?page_name=signup">Signup</a>
+            <a class="nav-link btn btn-primary rounded-pill px-5  me-3 mycss-stylebtn" href="?page_name=signup">Signup</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link btn btn-primary rounded-pill px-5  mycss-stylebtn" href="?page_name=login">Login</a>
+            <a class="nav-link btn btn-primary rounded-pill px-5 mycss-stylebtn  mycss-stylebtn1" href="?page_name=login">Login</a>
           </li>
         </ul>
       </div>
