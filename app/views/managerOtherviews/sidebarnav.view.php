90f0977c2ef4738725b502b06a61273cb8a233e8
alexokemwa <okemwanalex@gmail.com>
2024-03-15 11:18:52 +0300

    created manager sidebar nav

---
+++ app/views/managerOtherviews/sidebarnav.view.php
@@ -0,0 +1,58 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar myclassstylesidebar">
+              <div class="sidebar-sticky mystylesidebarsticky">
+                <ul class="nav flex-column ">
+                  <li class="nav-item">
+                    <a class="nav-link  mystlenav" href="?page_name=home">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    farmers</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=orders">orders</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=payments">payments</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=report">report</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=account">account</a>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    employees</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=deliveries">deliveries</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=payment">payment</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=reports">reports</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=accounts">accounts</a>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    communication</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=email">emails</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=message">message</a>
+                      </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    c_property</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=vehicles">vehicles</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=vehiclepack">vehiclepack</a>
+                     </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle mystlenav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    account</a>
+                    <ul class="dropdown-menu">
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=view">view</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=update">update</a>
+                      <a class="nav-link dropdown-item mystlenav" href="?page_name=delete">delete</a>
+                      </ul>
+                  </li>
+                </ul>
+                </ul>
+              </div>
+            </nav>
+           
