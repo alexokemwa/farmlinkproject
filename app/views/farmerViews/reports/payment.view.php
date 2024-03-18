67edc6fd4c126c68e560b455e957816c25f60037
alexokemwa <okemwanalex@gmail.com>
2024-03-18 22:33:16 +0300

    added farmer orders contrller files

--- app/views/farmerViews/reports/payment.view.php
+++ app/views/farmerViews/reports/payment.view.php
@@ -2,8 +2,19 @@
 //the navbar of home farmers view is requiresd in all farmers view pages
 require views_path("farmerOtherviews/constantnavview");?>
 <div class="container-fluid myclassmargintop" >
-        <h1>payment report</h1>
+        <h1>markets</h1>
         <h4><?php echo APP_NAME;?></h4>
+        <div class="row">
+            <div class="col-2 col-lg d-none d-lg-block"> <!-- Apply classes to hide on smaller screens -->
+                <?php require views_path("farmerOtherviews/farmerreportssidebar");?>
+           </div> 
+            <div class="col-12 col-lg-10 mystylemainbodyfullview"> <!-- Adjust columns for larger screens -->
+            
+            </div>
+            </div>
+      
+     
+   
     </div>
 
 <?php require views_path("otherviews/footer");?>
