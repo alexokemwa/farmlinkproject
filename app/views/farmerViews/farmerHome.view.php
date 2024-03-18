662240cea94e2e3be09dc6d49ca3b7e3e5252314
alexokemwa <okemwanalex@gmail.com>
2024-03-18 22:33:47 +0300

    modified farmer views

--- app/views/farmerViews/farmerHome.view.php
+++ app/views/farmerViews/farmerHome.view.php
@@ -1,9 +1,16 @@
 <?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    header("Location: index.php?page_name=login");
+ }
+
+?>
+<?php
 //the navbar of home farmers view is requiresd in all farmers view pages
 require views_path("farmerOtherviews/constantnavview");?>
 <div class="container-fluid myclassmargintop" >
         <h1>(name) welcome back!</h1>
-        <h4>homepage</h4>
+        <h4><span>homepage </span><span class="mystlyehomeorderbtn"><a href="?page_name=makeorder" >make order</a></span></h4> 
         <div class="row">
             <div class="col-2 col-lg d-none d-lg-block"> <!-- Apply classes to hide on smaller screens -->
                 <?php require views_path("farmerOtherviews/farmerhomesidebar1nav");?>
--- app/views/farmerViews/markets.view.php
+++ app/views/farmerViews/markets.view.php
@@ -4,7 +4,16 @@ require views_path("farmerOtherviews/constantnavview");?>
 <div class="container-fluid myclassmargintop mystylemainbodyfullview" >
         <h1>markets</h1>
         <h4><?php echo APP_NAME;?></h4>
-    </div>
+        <div class="row d-flex flex-column-sm">
+            <div class="col-lg-6">
+                order farminputs
+            </div>
+            <div class="col-lg-6">
+                dispatch goods for
+            </div>
+        </div>
+
+</div>
 
 <?php require views_path("otherviews/footer");?>
 <?php require views_path("farmerOtherviews/pageFooter");?>
