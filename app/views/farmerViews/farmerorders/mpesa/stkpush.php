381fbba848bdc392bf53c5183da540f93c14715e
alexokemwa <okemwanalex@gmail.com>
2024-03-19 16:24:11 +0300

    stkpush change to match server link

--- app/views/farmerViews/farmerorders/mpesa/stkpush.php
+++ app/views/farmerViews/farmerorders/mpesa/stkpush.php
@@ -3,7 +3,7 @@
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://cdd3-102-216-154-20.ngrok-free.app/farmlinkproject/app/views/farmerViews/farmerorders/mpesa/darajacallback/callback.php';
+$callbackurl = 'https://ruralfarmlink.online/farmlinkproject/app/views/farmerViews/farmerorders/mpesa/darajacallback/callback.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
