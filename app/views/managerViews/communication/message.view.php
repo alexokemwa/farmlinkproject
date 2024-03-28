556eb5ff9e5c0a52b6698e2cd37831d445aedd17
alexokemwa <okemwanalex@gmail.com>
2024-03-28 07:19:06 +0300

    modified files

--- app/views/managerViews/communication/message.view.php
+++ app/views/managerViews/communication/message.view.php
@@ -2,7 +2,7 @@
 
   <?php require views_path("managerOtherviews/nav");?>
     <div class="container-fluid myclassmargintop">
-        <h1>comm msg management</h1>
+        <h1>msg communication management</h1>
         <div class="row">
            <div class="col-2 col-lg d-none d-lg-block"> <!-- Apply classes to hide on smaller screens -->
               <?php require views_path("managerOtherviews/sidebarnav");?>
@@ -10,7 +10,54 @@
 
            </div> 
            <div class="col-12 col-lg-10 mystylemainbodyfullview"> <!-- Adjust columns for larger screens -->
-           
+           <section class="section-2">
+            <h2>messages</h2>
+            <div>
+            <div class="row px-1">
+<?php require "../app/core/database.php"; ?>
+
+                         <?php
+                         
+                         error_reporting(E_ALL);
+                         ini_set('display_errors', 1);
+                         
+                              $sql = "SELECT * FROM  contactus";
+                              $all_product = mysqli_query($conn,$sql);
+                              if(mysqli_num_rows($all_product) > 0){
+                              while($row = mysqli_fetch_assoc($all_product)){
+                                   $message_id = $row ['contact_id'];
+                                   $sender = $row ['full_name'];
+                                   $sender_email = $row ['email'];
+                                   $message = $row ['message'];
+                                   echo"
+                                   <div class='card text-center'>
+                                    <div class='card-header'>
+                                    <span>msg no::</span>$message_id
+                                    </div>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'><span>sender:</span>$sender</h5>
+                                        <p class='card-text'><span>message:</span>$message</p>
+                                        <a href='mailto:$sender_email' class='btn btn-primary'>reply</a>
+                                    </div>
+                                    <div class='card-footer text-body-secondary'>
+                                    $sender_email
+                                    </div>
+                                    </div>";
+                                   
+                              }}
+                              else{
+                                   echo "
+                                   <h5 class='card-card'><span>no message yet!!</span></h5>
+                                   
+                                   ";
+                                }
+                              
+                    
+                         ?>
+                         
+                    </div>  
+            </div>
+        </section>
            </div>
         </div>
       
--- app/views/otherviews/header.view.php
+++ app/views/otherviews/header.view.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo esc(APP_NAME);?></title>
     <!-- link parts change on cloud implementations -->
-      <link rel="stylesheet" href="../asserts/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../asserts/css/all.min.css">
+    <!-- <link rel="stylesheet" href="../asserts/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../asserts/css/all.min.css"> -->
     <link rel="stylesheet" href="../asserts/css/custom.css">
     <link rel="stylesheet" href="../asserts/css/managercustom.css">
     <link rel="stylesheet" href="../asserts/css/farmercustom.css">
     <link rel="stylesheet" href="../asserts/css/employeecustom.css">
     <link rel="stylesheet" href="../asserts/css/contact.css">
     <link rel="stylesheet" href="../asserts/css/print.css" media="print">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-     -->
+    
 </head>
 <body>
