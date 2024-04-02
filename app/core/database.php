e46cc317ef214860ca9f5fde41f5c03ee6adb616
alexokemwa <okemwanalex@gmail.com>
2024-04-02 17:16:57 +0300

    specified cloud database

--- app/core/database.php
+++ app/core/database.php
@@ -1,5 +1,14 @@
-
 <?php
+
+// $hostName = "localhost";
+// $dbUser = "root";
+// $dbPassword = "";
+// $dbName = "farmlink";
+// $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+// if (!$conn) {
+//     die("Something failed");
+// }
+
 
 $hostName = "localhost:3306";
 $dbUser = "ruralfarm_alex";
