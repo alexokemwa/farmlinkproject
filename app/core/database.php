54d3fb37f2db374791acc3649e48a67675b1adde
alexokemwa <okemwanalex@gmail.com>
2024-04-02 17:53:51 +0300

    cloud database addition

--- app/core/database.php
+++ app/core/database.php
@@ -1,22 +1,22 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "farmlink";
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-if (!$conn) {
-    die("Something failed");
-}
-
-
-// $hostName = "localhost:3306";
-// $dbUser = "ruralfarm_alex";
-// $dbPassword = "udmF55nsAsHiMDT";
-// $dbName = "ruralfarm_farmlink";
+// $hostName = "localhost";
+// $dbUser = "root";
+// $dbPassword = "";
+// $dbName = "farmlink";
 // $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 // if (!$conn) {
 //     die("Something failed");
 // }
+
+
+$hostName = "localhost:3306";
+$dbUser = "ruralfarm_alex";
+$dbPassword = "udmF55nsAsHiMDT";
+$dbName = "ruralfarm_farmlink";
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+if (!$conn) {
+    die("Something failed");
+}
 
 
